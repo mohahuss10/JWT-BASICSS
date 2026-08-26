9565170d81ad764d8c88fda99afef4e7958910cf
mohamed <mohamedhussein6500@gmail.com>
2026-08-26 20:55:51 +0300

    more implementation

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ require('express-async-errors');
 const express = require('express')
 const app = express()
 
+const mainRouter = require('./routes/main')
+
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 ///middlewares
+
+
 app.use(express.static('./public'))
 app.use(express.json())
+
+app.use('/api/v1',mainRouter)
 
 const port = process.env.PORT || 5000
 const start = async()=>{
